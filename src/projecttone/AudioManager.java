8e64b2ef4a3858b02902f391e44e0501db436472
Jobin Johnson <jobinrjohnson@gmail.com>
2017-08-04 07:04:31 +0530

    Basic audio manager with note Synthesizer

--- src/projecttone/AudioManager.java
+++ src/projecttone/AudioManager.java
@@ -4,10 +4,54 @@
  */
 package projecttone;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sound.midi.MidiChannel;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.Synthesizer;
+
 /**
  *
  * @author jobinrjohnson
  */
 public class AudioManager {
-    
+
+    public static Synthesizer synthesizer = null;
+    public static MidiChannel[] midiChannels = null;
+
+    public enum Instruments {
+        PIANO,SOMETHING_ELSE
+    }
+
+    public int getInstrumentMidiChannel(Instruments channel) {
+        return channel.ordinal();
+    }
+
+    public AudioManager() {
+        if (synthesizer != null && midiChannels != null) {
+            return;
+        }
+        try {
+            synthesizer = MidiSystem.getSynthesizer();
+            midiChannels = synthesizer.getChannels();
+        } catch (MidiUnavailableException ex) {
+            Logger.getLogger(AudioManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void play(int note, int volume, Instruments instrument, int duration) {
+        int channel = getInstrumentMidiChannel(instrument);
+        int playerVolume = 127 * 100 / volume;
+        try {
+            synthesizer.open();
+            midiChannels[channel].noteOn(note, playerVolume); // C note
+            Thread.sleep(duration);
+            midiChannels[channel].noteOff(note);
+            synthesizer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
