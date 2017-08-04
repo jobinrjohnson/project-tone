9b45ad3e60659ac7fe34dd59e47f67ade1f73ca9
Jobin Johnson <jobinrjohnson@gmail.com>
2017-08-04 05:50:53 +0530

    Basic Structure

---
+++ src/projecttone/StandardSet.java
@@ -0,0 +1,13 @@
+/*
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ */
+package projecttone;
+
+/**
+ *
+ * @author jobinrjohnson
+ */
+public class StandardSet {
+    
+}
