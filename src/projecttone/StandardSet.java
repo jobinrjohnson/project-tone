0bf7102d46fa431a8b51b6e1f49682799dbddd59
Jobin Johnson <jobinrjohnson@gmail.com>
2017-08-05 18:49:52 +0530

    added notes in standard set

--- src/projecttone/StandardSet.java
+++ src/projecttone/StandardSet.java
@@ -4,10 +4,23 @@
  */
 package projecttone;
 
+import java.util.Arrays;
+
 /**
  *
  * @author jobinrjohnson
  */
 public class StandardSet {
-    
+
+    public static final String[] Notes = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
+
+    public int getToneNumber(String Note, int octave) {
+        int index = Arrays.asList(Notes).indexOf(Notes);
+        if (index > -1) {
+            int mOctave = octave < 12 && octave >= 0 ? octave : 0;
+            return (mOctave * 12) + index;
+        }
+        return 0;
+    }
+
 }
