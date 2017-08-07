7346e772e9cf72cf948c9417aec8e98dd595bae4
Jobin Johnson <jobinrjohnson@gmail.com>
2017-08-07 14:19:18 +0530

    set to decode an instant array

--- src/projecttone/Tone.java
+++ src/projecttone/Tone.java
@@ -4,6 +4,11 @@
  */
 package projecttone;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sound.midi.MidiChannel;
+import static projecttone.AudioManager.midiChannels;
+
 /**
  *
  * @author jobinrjohnson
@@ -21,12 +26,28 @@ public class Tone {
         public int octave;
         public int duration_ms;
         public long instant;
-        
+        public int velocity = 120;
+        public AudioManager.Instruments instrument;
     }
     
     public void playTone(Instant[] instants){
+        final MidiChannel[] midiChannels = AudioManager.midiChannels;
         for(Instant i : instants){
             new Thread(){
+
+                @Override
+                public void run() {
+                    super.run(); 
+                    int channel = AudioManager.getInstrumentMidiChannel(i.instrument);
+                    int toneNumber = StandardSet.getToneNumber(i.note, i.octave);
+                    midiChannels[channel].noteOn(toneNumber, i.velocity); 
+                    try {
+                        Thread.sleep(i.duration_ms);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(Tone.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                    midiChannels[channel].noteOff(toneNumber);
+                }
                 
             }.start();
         }
